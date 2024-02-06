ac95ac3ec9dbf4bfa45d521c77ef335888a4475d
Miguel Reyes <asiessoyeltier1@gmail.com>
2024-02-05 20:17:16 -0500

    [dev] Darkness and moon created - Sunrise

--- Sunrise/sketch.js
+++ Sunrise/sketch.js
@@ -1,12 +1,11 @@
-var groundHeight;
 var mountain1;
 var mountain2;
 
-var tree;
+let tree, moon; 
+let sun, darkness;
 
-var moon;
-var sun;
-var darkness;
+let groundHeight;
+
 
 function setup()
 {
@@ -44,8 +43,15 @@ function setup()
 		y: 70,
 		diameter: 80,
 	};
-    
-    //TASK: intialise a moon object with an extra property for brightness
+
+	moon = 
+	{
+		x: 720,
+		y: 90,
+
+		diameter: 55,
+		opacity: 255,
+	};
 
 
 	//set the initial darkness
@@ -59,11 +65,17 @@ function draw()
 	//TASK: update the values for the moons brightness, the sun's position and the darkness.
 	//You can either map this to the mouse's location (i.e. the futher left the mouse is the more daylight) or you can just change the values gradually over time.
 
-    sun.y = map(mouseX, 0,width,0,height);
+    sun.y = map(mouseX, 0,width,140,height);
 
 	//draw the sky
 	background(150, 200, 255);
 	noStroke();
+
+	// Moon
+	moon.opacity = map(mouseX, 0, width, 90, 255);
+	
+	fill(234, 234, 234, moon.opacity);
+	ellipse(moon.x, moon.y, moon.diameter);
 
 	//draw the sun
 	fill(255, 255, 0);
@@ -84,13 +96,12 @@ function draw()
 	triangle(mountain2.x, mountain2.y,
 		mountain2.x + mountain2.width, mountain2.y,
 		mountain2.x + (mountain2.width / 2), mountain2.y - mountain2.height);
-    
     //TASK: You can draw the tree yourself
-    
 
 	//TASK: make the scene dark by drawing a rectangle that covers the whole canvas.
 	//Use the alpha value of fill to determine how dark to make it
 
-
+	fill(13,13,255, map(mouseX, 0,width,0,110));
+	quad(0,0,width,0, width, height, 0, height);
 
 }
